b3226e08c715acf74801f080c5be3a70a2ca433e
VaultBoy <56277764+VaultBoy3922@users.noreply.github.com>
2025-05-13 23:48:13 -0400

    finished DiscordClass config update
    This finished the code rewrite needed, and code cleanup, for the discord bot to use the new YAML file. This should allow it to have a new text group added in config file, then look for messages mentioning the bot in the corresponding  discord channel. This should work the same as before, just you will be able to add new groups through the config file. Have not tested this yet, but the initial code is finished.

--- src/DiscordClass.py
+++ src/DiscordClass.py
@@ -9,13 +9,14 @@ from rich.traceback import install
 
 import TwilioClass
 import NocoClass
-from config import load_discord_bot_config
+from config import load_discord_bot_config, load_text_update_groups
 
 
 install(show_locals=True)
 TwilioClass = TwilioClass.TwilioClient()
 NocoClass = NocoClass.NocoClass()
 config_data = load_discord_bot_config()
+update_groups = load_text_update_groups()
 
 
 class MyClient(discord.Client):
@@ -42,36 +43,30 @@ class MyClient(discord.Client):
         else:
             return False
 
-    def __check_channel_id(self, message_channel: int, channel_id: int):
-        print(f"Message channel: {message_channel}")
-        print(f"Channel id: {channel_id}")
-        if str(message_channel) == str(channel_id):
-            return True
-        else:
-            return False
+    # def __check_channel_id(self, message_channel: int, channel_id: int):
+    #     print(f"Message channel: {message_channel}")
+    #     print(f"Channel id: {channel_id}")
+    #     if str(message_channel) == str(channel_id):
+    #         return True
+    #     else:
+    #         return False
 
-    def __send_message_to_subscribers(self, message_content, group=None):
+    def __send_message_to_subscribers(self, message_content, group):
         self.message_content = message_content
         NocoClass.authorize()
         print(f"Message content: {self.message_content}")
         self.message_content = self.message_content.replace(f"<@&{self.bot_id}> ", "")
         print(f"Message content: {self.message_content}")
-        if group == "worship":
-            print(f"Group: {group}")
-            for i in NocoClass.subscriber_list:
-                print(f"Subscriber: {i}")
-                if "Worship" in i["Updates Group"]:
-                    print(f"Sending message to {i['PhoneNumber']}")
-                    TwilioClass.send_message(
-                        body=self.message_content, to=f"+{i['PhoneNumber']}"
-                    )
-                    time.sleep(1)
-        else:
-            for i in NocoClass.subscriber_list:
+
+        for i in NocoClass.subscriber_list:
+            if group in i[f"{NocoClass.subscriber_type_column}"]:
+                print(f"Sending message to {i['PhoneNumber']}")
                 TwilioClass.send_message(
                     body=self.message_content, to=f"+{i['PhoneNumber']}"
                 )
-                time.sleep(1)
+            else:
+                print(f"Subscriber {i['PhoneNumber']} is not in group {group}")
+            time.sleep(1)
 
     async def on_ready(self):
         guild = discord.utils.get(self.guilds, name=self.guild)
@@ -83,54 +78,25 @@ class MyClient(discord.Client):
 
     # This event triggers when a message is sent in the Announcements channel
     # and the bot is mentioned
-    # TODO: rewrite this to use the new config for update groups. Will need to search the TextKeywords group for the values it looks for now
     # TODO: add a check to see if bot needs to add text infront of the message to indicate which group it comes from
     async def on_message(self, message):
-
-        # print(f"Message from {message.author}: {message.content}")
-        # print(message.channel.id)
-        # print(self.message)
         self.message = message
-        self.ANNOUNCEMENT_CHANNEL_ID = os.environ["ANNOUNCEMENT_CHANNEL_ID"]
-        self.WORSHIP_CHANNEL_ID = os.environ["WORSHIP_CHANNEL_ID"]
-
-        # print(f"Announcement channel id: {self.ANNOUNCEMENT_CHANNEL_ID}")
-        # print(f"Channel id: {self.message.channel.id}")
-        # print(f"Message mentions: {self.message.content.mentions}")
-        # print(self.message.content)
-        # Ignore messages from the bot itself
 
         if self.message.author == self.user:
             print("Message from bot")
             return
-        # print(self.message.channel.id)
-        # print(self.ANNOUNCEMENT_CHANNEL_ID)
-
-        # Check if the message is in the Announcements channel
-        channel_id_check = self.__check_channel_id(
-            message.channel.id, self.ANNOUNCEMENT_CHANNEL_ID
-        )
-        # print(f"Channel id check: {channel_id_check}")
-        # Check if the bot is mentioned
-        check_if_mentioned = self.__check_if_mentioned(message.content)
-        print(f"Check if mentioned: {check_if_mentioned}")
-
-        if self.__check_channel_id(message.channel.id, self.ANNOUNCEMENT_CHANNEL_ID):
-            print(f"check for channel id")
-            if self.__check_if_mentioned(message.content):
-                print(f"check for mention")
-                self.__send_message_to_subscribers(message.content)
-            else:
-                print("Not a mention")
-                return
-        elif self.__check_channel_id(message.channel.id, self.WORSHIP_CHANNEL_ID):
-            print(f"check for channel id")
-            if self.__check_if_mentioned(message.content):
-                print(f"check for mention")
-                self.__send_message_to_subscribers(message.content, group="worship")
-            else:
-                print("Not a mention")
-                return
+        if self.__check_if_mentioned(self.message.content):
+            print("Message is a mention")
         else:
+            print("Message is not a mention")
             return
-        print(f"Message from {message.author}: {message.content}")
+        for keyword, group_data in update_groups.text_update_groups.items():
+            print(f"Keyword: {keyword}")
+            print(f"Group data: {group_data}")
+            if group_data.discord_channel_id == self.message.channel.id:
+                print(f"Message is in a update group channel")
+                self.__send_message_to_subscribers(
+                    self.message.content, group=group_data.nocodb_tag
+                )
+            else:
+                print("Message is not in a update group channel")
